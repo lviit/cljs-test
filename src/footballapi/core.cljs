71027570ca69253c8b631bde43bb55b1102fa505
lviit <lauriviitala3000@gmail.com>
2019-11-10 15:54:10 +0200

    initial commit

---
+++ src/footballapi/core.cljs
@@ -0,0 +1,43 @@
+(ns footballapi.core
+  (:require-macros [cljs.core.async.macros :refer [go]])
+  (:require [reagent.core :as r]
+            [cljs-http.client :as http]
+            [cljs.core.async :refer [<!]]))
+            ;;[cljss.core :refer [defstyles]]))
+
+(enable-console-print!)
+
+;; define your app data so that it doesn't get over-written on reload
+(defonce matches (atom []))
+
+  (defonce requestmatches (go (let [response (<! (http/get "https://api.football-data.org/v2/competitions/CL/matches" {:with-credentials? false :headers {"X-Auth-Token" ""}}))]
+    ;(prn (:matches (:body response)))
+    (reset! matches (:matches (:body response))))))
+
+(defn match [match-data]
+  (fn []
+    [:div
+    [:p
+     (get-in match-data [:homeTeam :name])
+     " - "
+     (get-in match-data [:awayTeam :name])]]))
+
+(defn matches-list []
+  [:div
+   (for [match-data @matches]
+     ^{:key (:id match-data)} [match match-data])])
+
+(defn mountpoint []
+  (.getElementById js/document "app"))
+
+(defn mountit []
+  (r/render [matches-list]
+            (mountpoint)))
+
+(mountit)
+
+(defn on-js-reload []
+  ;; optionally touch your app-state to force rerendering depending on
+  ;; your application
+  ;; (swap! app-state update-in [:__figwheel_counter] inc)
+)
