d4a262206dd4f89328db922cbd9dc5661877e306
lviit <lauriviitala3000@gmail.com>
2020-02-08 19:19:44 +0200

    switch to shadow-cljs

--- src/football_api/core.cljs
+++ src/football_api/core.cljs
@@ -1,4 +1,4 @@
-(ns footballapi.core
+(ns football-api.core
   (:require-macros [cljs.core.async.macros :refer [go]])
   (:require [reagent.core :as r]
             [cljs-http.client :as http]
@@ -10,9 +10,9 @@
 ;; define your app data so that it doesn't get over-written on reload
 (defonce matches (r/atom []))
 
-  (defonce requestmatches (go (let [response (<! (http/get "https://api.football-data.org/v2/competitions/PL/matches" {:with-credentials? false :headers {"X-Auth-Token" ""}}))]
-    ;(prn (:matches (:body response)))
-    (reset! matches (:matches (:body response))))))
+(defonce requestmatches (go (let [response (<! (http/get "https://api.football-data.org/v2/competitions/PL/matches" {:with-credentials? false :headers {"X-Auth-Token" ""}}))]
+  ;(prn (:matches (:body response)))
+                             (reset! matches (:matches (:body response))))))
 
 (defn match [match-data]
   (fn []
@@ -20,25 +20,19 @@
       [:div.home (get-in match-data [:homeTeam :name])]
       [:div.score (get-in match-data [:score :fullTime :homeTeam]) " - " (get-in match-data [:score :fullTime :awayTeam])]
       [:div.away (get-in match-data [:awayTeam :name])]
-      [:div.date (:utcDate match-data)]
-    ]))
+      [:div.date (:utcDate match-data)]]))
+
 
 (defn matches-list []
   [:div
    (for [match-data @matches]
      ^{:key (:id match-data)} [match match-data])])
 
-(defn mountpoint []
-  (.getElementById js/document "app"))
-
-(defn mountit []
+(defn ^:dev/after-load start
+  []
   (r/render [matches-list]
-            (mountpoint)))
+    (.getElementById js/document "root")))
 
-(mountit)
-
-(defn on-js-reload []
-  ;; optionally touch your app-state to force rerendering depending on
-  ;; your application
-  ;; (swap! app-state update-in [:__figwheel_counter] inc)
-)
+(defn ^:export init
+  []
+  (start))
